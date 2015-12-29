f88712b2f271c646cfa17106a8fd396444670081
xiaofan <zeng-dream@live.com>
2015-12-29 18:29:45 +0800

    hashSet中添加可变对象

--- src/java1/util/Iterator_Study.java
+++ src/java1/util/Iterator_Study.java
@@ -22,6 +22,8 @@ public class Iterator_Study {
             System.out.println(it.next() + ""); // Java Abstract hello
             it.remove();
             // 迭代时不能【有效】修改集合元素，报异常ConcurrentModificationException
+            // 如果HashSet包含的是对象，则对象不能修改，但对象内的属性可以修改。
+            // 当向hashSet中添加可变对象时，如果修改其对象，有可能导致该对象与集合内其他对象相同，导致HashSet无法准确访问该对象
             col.remove("Java"); // 此行不会报错，因为it.remove()首先移除的是"Java"
             // col.remove("hello");
         }
