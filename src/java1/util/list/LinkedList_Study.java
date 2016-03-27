0ddadcd3f7f99bc38ad4b70c2fefb425914ea003
zxiaofan <zeng-dream@live.com>
2016-03-27 14:11:31 +0800

    LinkedList:remove int本身

--- src/java1/util/list/LinkedList_Study.java
+++ src/java1/util/list/LinkedList_Study.java
@@ -27,13 +27,15 @@ public class LinkedList_Study {
         // contains调用int indexOf()方法，先判断入参是否是null，是则x.item == null，不是则equals比较，最后返回index或-1
         System.out.println(list.contains(null)); // true
         list.remove(null); // 只remove第一个元素
+        list.add(1);
+        list.remove((Integer) 1); // remove int本身
         list.add("a");
         list.add("b");
         Iterator ite = list.iterator();
         while (ite.hasNext()) {
             Object object = (Object) ite.next();
             System.out.println(object);
-            list.remove(6); // 迭代时remove抛ConcurrentModificationException
+            list.remove("a"); // 迭代时remove抛ConcurrentModificationException
         }
     }
 }
