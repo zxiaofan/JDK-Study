4e124da069fb6c1bd7ed93878efb543e11762f08
zxiaofan <zeng-dream@live.com>
2016-03-27 14:09:21 +0800

    集合总结：LinkedList迭代时remove抛ConcurrentModificationException; contains实现；remove

---
+++ src/java1/util/list/LinkedList_Study.java
@@ -0,0 +1,39 @@
+/*
+ * 文件名：LinkedList_Study.java
+ * 版权：Copyright 2007-2016 517na Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： LinkedList_Study.java
+ * 修改人：xiaofan
+ * 修改时间：2016年3月27日
+ * 修改内容：新增
+ */
+package java1.util.list;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * @author xiaofan
+ */
+public class LinkedList_Study {
+    @Test
+    public void test() {
+        List list = new LinkedList<>();
+        list.add(null);
+        list.add(null); // 允许null，且允许重复
+        System.out.println(list); // [null, null]
+        // contains调用int indexOf()方法，先判断入参是否是null，是则x.item == null，不是则equals比较，最后返回index或-1
+        System.out.println(list.contains(null)); // true
+        list.remove(null); // 只remove第一个元素
+        list.add("a");
+        list.add("b");
+        Iterator ite = list.iterator();
+        while (ite.hasNext()) {
+            Object object = (Object) ite.next();
+            System.out.println(object);
+            list.remove(6); // 迭代时remove抛ConcurrentModificationException
+        }
+    }
+}
