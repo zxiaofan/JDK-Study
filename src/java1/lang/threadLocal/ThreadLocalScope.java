d6009d7529d82d2cb0522a95f46f7291aacc9c6a
xiaofan <zeng-dream@live.com>
2017-05-05 11:31:49 +0800

    【Note】SimpleDateFormat线程不安全，Java7及以下建议使用线程安全的工具类（{未使用Spring等方式实现单例时可}每个类new一个，但绝不能加static，不能传入多线程使用），【如果使用了Spring注入】由于Spring导致类单例，So即便每个类new一个非static的SimpleDateFormat，也是线程不安全的（即便非static的基本类型<如int>也只会初始化一次）。

--- src/java1/lang/threadLocal/ThreadLocalScope.java
+++ src/java1/lang/threadLocal/ThreadLocalScope.java
@@ -25,6 +25,12 @@ import org.junit.Test;
  * 
  * local拥有的AtomicInteger将重新初始化，即便AtomicInteger是全局static（其他类的public）也将重新初始化。
  * 
+ * 【Note】SimpleDateFormat线程不安全，Java7及以下建议使用线程安全的工具类（{未使用Spring等方式实现单例时可}每个类new一个，但绝不能加static，不能传入多线程使用），
+ * 
+ * 【如果使用了Spring注入】由于Spring导致类单例，So即便每个类new一个非static的SimpleDateFormat，也是线程不安全的（即便非static的基本类型<如int>也只会初始化一次）。
+ * 
+ * 此处不包含Spring注入时的测试代码。
+ * 
  * @author zxiaofan
  */
 public class ThreadLocalScope {
@@ -57,21 +63,21 @@ public class ThreadLocalScope {
         System.out.println(local.get()); // 0:103128921
         ExecutorService service = Executors.newFixedThreadPool(nThreads);
         // MyRunnable执行顺序不定，So可能出现name2:1:XXXXX
-        service.submit(new MyRunnable("name1", local)); // name1_1:103128926
+        service.submit(new MyRunnable("name1", local)); // name1_age1_statics1_1:103128926
         sleep(10); // 休眠10ms，避免多线程时毫秒精度不够
-        service.submit(new MyRunnable("name2", local)); // name2_2:103128936
+        service.submit(new MyRunnable("name2", local)); // name2_age1_statics11_2:103128936
         sleep(10);
-        service.submit(new MyRunnable("name3", local)); // name3_3:103128946
+        service.submit(new MyRunnable("name3", local)); // name3_age1_statics111_3:103128946
         sleep(2000);
-        service.submit(new MyRunnable2("Hi-4", local)); // Hi-4_4:103130949
+        service.submit(new MyRunnable2("Hi-4", local)); // Hi-4_age2_statics2_4:103130949
         sleep(10);
         // 【Note】当nThreads=4时，按序第5次执行的Runnable打印出的AtomicInteger不在是递增后的5而是重新初始化后的1
         // 【待续】只有深入线程池底层才能解释其原因了
-        service.submit(new MyRunnable2("Hi-5", local)); // Hi-5_1:103128926
+        service.submit(new MyRunnable2("Hi-5", local)); // Hi-5_age2_statics22_1:103128926
         sleep(2000);
-        service.submit(new MyRunnable3("Boy-6", local)); // Boy-6_2:103128936
+        service.submit(new MyRunnable3("Boy-6", local)); // Boy-6_age1_statics3_2:103128936
         sleep(10);
-        service.submit(new MyRunnable3("Boy-7", local)); // Boy-7_3:103128946
+        service.submit(new MyRunnable3("Boy-7", local)); // Boy-7_age1_statics33_3:103128946
         service.shutdown();
     }
 
@@ -103,6 +109,21 @@ class MyRunnable implements Runnable {
     /**
      * 添加字段注释.
      */
+    private String age = "1";
+
+    /**
+     * staticParam.
+     */
+    private static String staticParam = "s1";
+
+    /**
+     * staticParam.
+     */
+    private static int staticint = 1;
+
+    /**
+     * 添加字段注释.
+     */
     private ThreadLocal<String> local;
 
     /**
@@ -124,7 +145,12 @@ class MyRunnable implements Runnable {
      */
     @Override
     public void run() {
-        System.err.println(name + "_" + local.get());
+        // age每次输出都相同（1），static每次输出都会拼接一个1【static变量只初始化一次，static的基本类型如int也只会初始化一次】
+        System.err.println(name + "_" + "age" + age + "_" + "static" + staticParam + "_" + local.get());
+        age += "1";
+        staticParam += "1";
+        System.err.println("staticint:" + staticint);
+        staticint++;
     }
 
 }
@@ -138,6 +164,16 @@ class MyRunnable2 implements Runnable {
      * 添加字段注释.
      */
     private String name;
+
+    /**
+     * 添加字段注释.
+     */
+    private String age = "2";
+
+    /**
+     * staticParam.
+     */
+    private static String staticParam = "s2";
 
     /**
      * 添加字段注释.
@@ -163,7 +199,9 @@ class MyRunnable2 implements Runnable {
      */
     @Override
     public void run() {
-        System.err.println(name + "_" + local.get());
+        System.err.println(name + "_" + "age" + age + "_" + "static" + staticParam + "_" + local.get());
+        age += "2";
+        staticParam += "2";
     }
 
 }
@@ -177,6 +215,16 @@ class MyRunnable3 implements Runnable {
      * 添加字段注释.
      */
     private String name;
+
+    /**
+     * 添加字段注释.
+     */
+    private String age = "1";
+
+    /**
+     * staticParam.
+     */
+    private static String staticParam = "s3";
 
     /**
      * 添加字段注释.
@@ -202,7 +250,9 @@ class MyRunnable3 implements Runnable {
      */
     @Override
     public void run() {
-        System.err.println(name + "_" + local.get());
+        System.err.println(name + "_" + "age" + age + "_" + "static" + staticParam + "_" + local.get());
+        age += "3";
+        staticParam += "3";
     }
 
 }
